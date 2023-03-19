91c39d2ce42ee477cdbee8c0cc2ae681e2610237
Druvasena Reddy Naini <nainidruvasenareddy3737@gmail.com>
2023-03-20 01:00:03 +0530

    Pizza-billing

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -3,7 +3,7 @@ package com.driver;
 public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
-        super(isVeg);
+        super(isVeg,true);
         // your code goes here
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,32 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    public boolean isDeluxe = false;
+    private int extraCheese, extraToppings, takeAway;
+
+    public void setDeluxe(boolean isDeluxe) {
+        this.isDeluxe = isDeluxe;
+    }
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+
+        if(isVeg) price = 300;
+        else price = 400;
+
+    }
+    public Pizza(Boolean isVeg,Boolean isDeluxe){
+        this.isVeg = isVeg;
+        // your code goes here
+
+        if(isVeg) price = 300;
+        else price = 400;
+
+        if(isDeluxe){
+            addExtraCheese();
+            addExtraToppings();
+        }
     }
 
     public int getPrice(){
@@ -17,18 +39,32 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        extraCheese = 80;
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(extraCheese!=0) {
+            if (isVeg) extraToppings = 70;
+            else extraToppings = 120;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        takeAway = 20;
     }
 
     public String getBill(){
         // your code goes here
+        bill = "Base Price Of The Pizza: "+price+"\n";
+        if(extraCheese!=0)
+            bill += "Extra Cheese Added: "+extraCheese+"\n";
+        if(extraToppings!=0)
+            bill += "Extra Toppings Added: "+extraToppings+"\n";
+        if(takeAway!=0)
+            bill += "Paperbag Added: "+takeAway+"\n";
+        bill += "Total Price: "+(price+extraCheese+extraToppings+takeAway);
         return this.bill;
     }
 }
